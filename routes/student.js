40f2506c7379c634b21290aca5f005ebd027275c
12srishti12 <srishtiparthsarthi@gmail.com>
2022-06-06 23:45:09 +0530

    get and post form created

--- routes/student.js
+++ routes/student.js
@@ -6,17 +6,23 @@ router.get('/', function(req, res, next) {
   console.log(req);
   res.json({name:"srishti_24"})
 });
+router.get('/data', function(req, res, next) {
+  res.json({name:req.query.fname,lname:req.query.lname})
+});
+router.post('/about', function(req, res, next) {
+  res.json({name:req.body.fname,lname:req.body.lname})
+});
  router.get('/:da-:ta', function(req, res, next) {
-   console.log(req.params.da);
-   res.json({name:req.params.da + req.params.ta});
+   console.log(req.params);
+   res.json({name:req.params.da + req.params.ta+"="+req.params.da+req.params.ta});
  });
-router.get('/:da', function(req, res, next) {
-  console.log(req.params.da);
-  res.json({name:req.params.da});
-});
-router.get('/:ta', function(req, res, next) {
-  console.log(req.params.ta);
-  res.json({name:req.params.ta});
-});
+// router.get('/:da', function(req, res, next) {
+//   console.log(req.params.da);
+//   res.json({name:req.params.da});
+// });
+// router.get('/:ta', function(req, res, next) {
+//   console.log(req.params.ta);
+//   res.json({name:req.params.ta});
+// });
 
 module.exports = router;
