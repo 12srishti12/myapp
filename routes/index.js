fff040de56655accfdfccc24ad3e2e06df1fa2ad
12srishti12 <srishtiparthsarthi@gmail.com>
2022-06-14 11:36:29 +0530

    coonecting kccdatabase

--- routes/index.js
+++ routes/index.js
@@ -3,55 +3,55 @@ var router = express.Router();
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "data_sr"
+  host: "easylearning.guru",
+  user: "kcc_student",
+  password: "Kccitm.edu.in1",
+  database: "kccStudent"
 });
 
+
 router.get('/', function(req, res, next) {
+   
     con.connect(function(err) {
         if (err) console.log("err");
-        console.log("Connected to mySQL!");
+        console.log("Connected!");
       });
+
     res.render('index')
 });
 
+// router.get('/getClass', function(req, res, next) {
+//     console.log(req.query)
+// res.json({"Name":req})
+// });
 router.post('/about', function(req, res, next) {
     console.log(req.body)
-    var sql = "INSERT INTO `data` (`fname`, `lname`) \
+    var sql = "INSERT INTO `srishti` (`fname`, `lname`) \
     VALUES ('"+req.body.fname+"', '"+req.body.lname+"');"
     console.log(sql)
    con.connect()
       con.query(sql, function (err, result) {
-        if (err) throw err;
+        if (err) console.log(err);
         console.log(result);
+        res.json({"Name":result})
 
       });
-      // var sql1 = "select * from `qqq`"
-      // con.query(sql1, function (err, result) {
-      //   if (err) throw err;
-      //   console.log(result);
-      // });
-res.json({"Name":req.body})
-});
-router.get('/data', function(req, res, next) {
-  console.log(req.query)
-  var sql = "INSERT INTO `k` (`name`, `college`) \
-  VALUES ('"+req.query.name+"', '"+req.query.college+"');"
-  console.log(sql)
- con.connect()
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log(result);
+     
 
-    });
-    // var sql1 = "select * from `qqq`"
-    // con.query(sql1, function (err, result) {
-    //   if (err) throw err;
-    //   console.log(result);
-    // });
-res.json({"Name":req.query})
 });
+
+   
+
 
 module.exports = router;
+
+// router.all('/getPost', function(req, res, next) {
+//     console.log(req.body)
+//     if(req.body){
+//         //do something
+//     }else{
+//         //do something else
+//     }
+//     console.log(req.body.pass)
+// res.json({"Name":req.body})
+// }
