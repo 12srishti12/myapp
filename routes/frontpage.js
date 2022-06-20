98f8b2cf9fde0e0d2b870c0bedce79837e870e8e
12srishti12 <srishtiparthsarthi@gmail.com>
2022-06-21 00:06:22 +0530

    add,multi changed

--- routes/frontpage.js
+++ routes/frontpage.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res, next) {
     var b=parseInt(req.body.num2);
     var c=a+b;
     res.render('frontpage',{c});
+
   });
   router.post('/multi', function(req, res, next) {
     var a=parseInt(req.body.num1);
